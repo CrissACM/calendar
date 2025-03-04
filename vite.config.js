ba0a9c1a327eed9f70140797de9fa38c6571f921
Crissacm <alex.ccm7Z@gmail.com>
2025-03-04 01:00:17 -0400

    feat: add service worker

--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,22 @@ export default defineConfig({
         globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
         cleanupOutdatedCaches: true,
         clientsClaim: true,
+        runtimeCaching: [
+          {
+            urlPattern: /^http:\/\/localhost:4000\/.*/i,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "CalendarJWT",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
       },
 
       devOptions: {
